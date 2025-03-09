17c9a1ee28a74e159a241a9107fa9610bd3dbdff
kanitvural <kanitvural@gmail.com>
2025-03-09 03:14:24 +0300

    implement test folder structure to cookiecutter template test folder

--- tests/functional_tests/test__makefile.py
+++ tests/functional_tests/test__makefile.py
@@ -5,12 +5,11 @@ from pathlib import Path
 def test__linting_passes(project_dir: Path):
     subprocess.run(["make", "lint-ci"], cwd=project_dir, check=True)
 
+
 def test__tests_pass(project_dir: Path):
     subprocess.run(["make", "install"], cwd=project_dir, check=True)
     subprocess.run(["make", "test-wheel-locally"], cwd=project_dir, check=True)
 
-
-def test__install_succeeds(): ...
 
 
 # Setup:
@@ -23,6 +22,4 @@ def test__install_succeeds(): ...
 
 # Clean up/Tear down:
 # 5. Remove the virtual environment
-# 6. Remove the generated project
-# 6. Remove the generated project
 # 6. Remove the generated project
